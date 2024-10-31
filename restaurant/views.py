bdc9c06dacfeaccf87c91740a7aa7569409c38ea
Vamshi <vamshiachavelli@gmail.com>
2024-10-31 11:33:04 -0500

    djoser token baes auth

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,15 +1,27 @@
 from rest_framework import generics, viewsets
 from .models import Menu, Booking
 from .serializers import MenuItemSerializer, BookingSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):  # Use this to combine Retrieve, Update, and Destroy
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()  # Fetch all booking objects
     serializer_class = BookingSerializer
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
