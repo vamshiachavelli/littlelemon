659a68460bc88035a80e2c93be05f997ba3b6aae
Vamshi <vamshiachavelli@gmail.com>
2024-11-06 22:34:24 -0600

    Anonymus user can add item to cart, cart_view, cart_remove

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Menu, Booking
 from django.contrib.auth.models import User
-
+from decimal import Decimal
+ 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
@@ -16,3 +17,36 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']  # Adjust fields as necessary
+'''class CartSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
+
+
+    def validate(self, attrs):
+        attrs['price'] = attrs['quantity'] * attrs['unit_price']
+        return attrs
+
+    class Meta:
+        model = Cart
+        fields = ['user', 'menuitem', 'unit_price', 'quantity', 'price']
+        extra_kwargs = {
+            'price': {'read_only': True}
+        }
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['order', 'menuitem', 'quantity', 'price']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    orderitem = OrderItemSerializer(many=True, read_only=True, source='order')
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'delivery_crew',
+                  'status', 'date', 'total', 'orderitem']'''
